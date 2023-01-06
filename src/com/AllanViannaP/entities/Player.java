6970e4bf6f29f1f5a1d1228153c11b01301283a3
Allan Vianna <119679121+AllanViannaP@users.noreply.github.com>
2023-01-06 20:46:19 +0900

    Base moves.

--- src/com/AllanViannaP/entities/Player.java
+++ src/com/AllanViannaP/entities/Player.java
@@ -4,8 +4,20 @@ import java.awt.image.BufferedImage;
 
 public class Player extends Entity {
 
+	public boolean right,up,left,down;
+	public double spd = 0.8;
+	
+	
 	public Player(int x, int y, int width, int height, BufferedImage sprite) {
 		super(x, y, width, height, sprite);
 	}
 
+	public void tick() {
+		if(right) {x+=spd;}
+		else if(left){x-=spd;}
+		if(up) {y-=spd;}
+		else if(down) {y+=spd;}
+	}
+	
+	
 }
