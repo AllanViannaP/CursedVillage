b22479eca062cc53c5fb2d893f43aebd34964fd4
Allan Vianna <119679121+AllanViannaP@users.noreply.github.com>
2023-01-22 03:27:09 -0300

    Enemy base animation code

--- src/com/AllanViannaP/entities/Enemy.java
+++ src/com/AllanViannaP/entities/Enemy.java
@@ -13,6 +13,10 @@ public class Enemy extends Entity{
 	
 	public double spd = 0.4;
 
+	public int right_dir = 0, left_dir = 1, up_dir = 2, down_dir = 3;
+	public int dir = right_dir;
+	private int frames = 0, maxFrames=15,index=0,maxIndex = 2;
+	
 	private BufferedImage[] BLOOD_PHANTOM_RIGHT;
 	private BufferedImage[] BLOOD_PHANTOM_LEFT;
 	private BufferedImage[] BLOOD_PHANTOM_UP;
@@ -24,10 +28,10 @@ public class Enemy extends Entity{
 	
 	public Enemy(int x, int y, int width, int height, BufferedImage sprite) {
 		super(x, y, width, height, sprite);
-		BLOOD_PHANTOM_RIGHT = new BufferedImage[2];
-		BLOOD_PHANTOM_LEFT = new BufferedImage[2];
-		BLOOD_PHANTOM_UP = new BufferedImage[2];
-		BLOOD_PHANTOM_DOWN = new BufferedImage[2];
+		BLOOD_PHANTOM_RIGHT = new BufferedImage[3];
+		BLOOD_PHANTOM_LEFT = new BufferedImage[3];
+		BLOOD_PHANTOM_UP = new BufferedImage[3];
+		BLOOD_PHANTOM_DOWN = new BufferedImage[3];
 		
 		for(int i=0;i<2;i++) {
 			BLOOD_PHANTOM_RIGHT[i] = Game.spritesheet.getSprite(160+(i*16), 0, 16, 16);
@@ -41,24 +45,46 @@ public class Enemy extends Entity{
 	public void tick() {
 		if((int)x<Game.player.getX() && World.isFree((int)(x+spd), this.getY())) {
 			x+=spd;
+			dir = right_dir;
 		}
 		else if((int)x>Game.player.getX() && World.isFree((int)(x-spd), this.getY())) {
 			x-=spd;
+			dir = left_dir;
 		}
 		if((int)y<Game.player.getY()&& World.isFree(this.getX(), (int)(y+spd))) {
 			y+=spd;
+			dir = up_dir;
 		}
 		else if((int)y>Game.player.getY()&& World.isFree(this.getX(), (int)(y-spd))) {
 			y-=spd;
+			dir = down_dir;
 		}
 		
 		
+			frames++;
+			if(frames == maxFrames) {
+				frames = 0;
+				index++;
+				if(index > maxIndex) {
+					index = 0;
+				}
+			
 		
-	}
+	}}
 	
 	public void render(Graphics g){
-		g.drawImage(BLOOD_PHANTOM_RIGHT[0], this.getX()-Camera.x,this.getY()-Camera.y,null);
+		if(dir == right_dir) {
+		g.drawImage(BLOOD_PHANTOM_RIGHT[index], this.getX()-Camera.x,this.getY()-Camera.y,null);}
+		else if(dir == left_dir) {
+		g.drawImage(BLOOD_PHANTOM_LEFT[index], this.getX()-Camera.x,this.getY()-Camera.y,null);
+		}
 		
+		if(dir == up_dir) {
+		g.drawImage(BLOOD_PHANTOM_UP[index], this.getX()-Camera.x,this.getY()-Camera.y,null);
+		}
+		else if(dir == down_dir) {
+		g.drawImage(BLOOD_PHANTOM_DOWN[index], this.getX()-Camera.x,this.getY()-Camera.y,null);
+		}
 	}
 
 }
